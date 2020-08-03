bc3c9e92fe51f73cf63a9b4de20709e27c1ce6ee
dsabene315 <42305829+dsabene315@users.noreply.github.com>
2020-08-03 16:49:43 -0400

    Update getProjectsByIssueTypeAndWorkflow.groovy

--- getProjectsByIssueTypeAndWorkflow.groovy
+++ getProjectsByIssueTypeAndWorkflow.groovy
@@ -12,7 +12,7 @@ WorkflowManager workflowManager = ComponentAccessor.getWorkflowManager()
 // Define variables
 def workflowName = "Simple Software Workflow"
 def issueTypeName = "Story"
-def IssueTypeId = "10000"
+def issueTypeId = "10000"
 
 // Get all issuetype schemes
 issueTypeSchemeManager.getAllSchemes()*.getAssociatedProjectObjects().each { projects ->
@@ -23,10 +23,10 @@ issueTypeSchemeManager.getAllSchemes()*.getAssociatedProjectObjects().each { pro
         if (project.getIssueTypes()*.name.contains(issueTypeName)) {
             
             // Get the workflow for the current project/issuetype pair
-            if (workflowManager.getWorkflow(project.id, IssueTypeId).getName().contains(workflowName)) {
+            if (workflowManager.getWorkflow(project.id, issueTypeId).getName().contains(workflowName)) {
                 
                 // Log the current project and its associated workflow
-                log.debug(project.name + " -> " + workflowManager.getWorkflow(project.id, IssueTypeId).getName())
+                log.debug(project.name + " -> " + workflowManager.getWorkflow(project.id, issueTypeId).getName())
             }    
         }
     }
